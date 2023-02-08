5ccfd7b22b04f38184344093fa44fb648fdb2816
Rui <rpmonteiro24@hotmail.com>
2023-02-08 09:36:00 +0000

    Initial commit

---
+++ ns-3.35/src/network/bindings/scan-header.h
@@ -0,0 +1,15 @@
+// -*- c++ -*-
+
+#include "ns3/network-module.h"
+
+using namespace ns3;
+
+namespace {
+void
+__foo ()
+{
+  static SequenceNumber32 a;
+  static SequenceNumber16 b;
+}
+
+} // namespace
