6c42e0480e41081afe25170ef85226f4a5a7733b
Rui <ruicunham@hotmail.com>
2023-09-26 13:44:51 +0100

    [Utils] Add option to generate full mesh topologies

--- utils/init-topo.py
+++ utils/init-topo.py
@@ -11,8 +11,13 @@ def create_nodes(topo_path):
         hosts = {}
         switches = {}
 
+        if args.is_mesh:
+            args.n_hosts = args.n_switches
+
         for i in range(args.n_hosts):
-            hosts[f"h{i+1}"] = {"type": "host"}
+            hosts[f"h{i+1}"] = {
+                "type": "host",
+            }
 
         for i in range(args.n_switches):
             switches[f"s{i+1}"] = {
@@ -60,13 +65,38 @@ def create_links(topo_path):
     with open(os.path.join(topo_path, "links.toml"), "w") as links_file:
         links = {}
 
-        for i in range(args.n_links):
-            links[f"l{i+1}"] = {
-                "edges": ["<node1>", "<node2>"],
-                "dataRate": "100Gbps",
-                "delay": "1ms",
-                "pcap": False,
-            }
+        if args.is_mesh:
+            link_count = 1
+
+            # Switch to Switch
+            for i in range(args.n_switches):
+                src = f"s{i+1}"
+                for j in range(i + 1, args.n_switches):
+                    links[f"l{link_count}"] = {
+                        "edges": [src, f"s{j+1}"],
+                        "dataRate": "100Gbps",
+                        "delay": "1ms",
+                        "pcap": False,
+                    }
+                    link_count += 1
+
+            # Switch to Host
+            for i in range(args.n_switches):
+                links[f"l{link_count}"] = {
+                    "edges": [f"s{i+1}", f"h{i+1}"],
+                    "dataRate": "100Gbps",
+                    "delay": "1ms",
+                    "pcap": False,
+                }
+                link_count += 1
+        else:
+            for i in range(args.n_links):
+                links[f"l{i+1}"] = {
+                    "edges": ["<node1>", "<node2>"],
+                    "dataRate": "100Gbps",
+                    "delay": "1ms",
+                    "pcap": False,
+                }
 
         toml.dump(links, links_file)
 
@@ -117,6 +147,12 @@ if __name__ == "__main__":
     parser.add_argument("--switches", dest="n_switches", type=int, default=1)
     parser.add_argument("--links", dest="n_links", type=int, default=2)
     parser.add_argument("--apps", dest="n_apps", type=int, default=1)
+    parser.add_argument(
+        "--mesh",
+        dest="is_mesh",
+        action=argparse.BooleanOptionalAction,
+        default=False,
+    )
 
     global args
     args = parser.parse_args()
