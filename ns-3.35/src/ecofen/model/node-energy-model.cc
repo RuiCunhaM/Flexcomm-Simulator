74993b88ec571350d2b5aceebe05dc37b090fd39
pedrommf7 <pedro2mferreira@gmail.com>
2024-02-29 18:47:55 +0000

    more accurate consumption values

--- ns-3.35/src/ecofen/model/node-energy-model.cc
+++ ns-3.35/src/ecofen/model/node-energy-model.cc
@@ -27,184 +27,187 @@
 #include "ns3/loopback-net-device.h"
 #include "ns3/core-module.h"
 
-namespace ns3 {
-
-NS_OBJECT_ENSURE_REGISTERED (NodeEnergyModel);
-
-TypeId
-NodeEnergyModel::GetTypeId (void)
+namespace ns3
 {
-  static TypeId tid =
-      TypeId ("ns3::NodeEnergyModel")
-          .SetParent<Object> ()
-          .AddConstructor<NodeEnergyModel> ()
-          .AddAttribute (
-              "NodeState", "The default state of the node (0=off, 1=on)", UintegerValue (1),
-              MakeUintegerAccessor (&NodeEnergyModel::SetNodeState, &NodeEnergyModel::GetNodeState),
-              MakeUintegerChecker<uint32_t> ());
-  return tid;
-}
 
-NodeEnergyModel::NodeEnergyModel ()
-    : m_nodeState (1), m_powerDrawn (0), m_lastConso (0), m_lastUpdate (Time (0))
-{
-}
+  NS_OBJECT_ENSURE_REGISTERED(NodeEnergyModel);
 
-NodeEnergyModel::~NodeEnergyModel ()
-{
-}
+  TypeId
+  NodeEnergyModel::GetTypeId(void)
+  {
+    static TypeId tid =
+        TypeId("ns3::NodeEnergyModel")
+            .SetParent<Object>()
+            .AddConstructor<NodeEnergyModel>()
+            .AddAttribute(
+                "NodeState", "The default state of the node (0=off, 1=on)", UintegerValue(1),
+                MakeUintegerAccessor(&NodeEnergyModel::SetNodeState, &NodeEnergyModel::GetNodeState),
+                MakeUintegerChecker<uint32_t>());
+    return tid;
+  }
 
-uint32_t
-NodeEnergyModel::GetNodeState (void) const
-{
-  return m_nodeState;
-}
+  NodeEnergyModel::NodeEnergyModel()
+      : m_nodeState(1), m_powerDrawn(0), m_lastConso(0), m_lastUpdate(Time(0))
+  {
+  }
 
-void
-NodeEnergyModel::SetNodeState (uint32_t nodeState)
-{
-  //NS_LOG_FUNCTION (this << nodeState);
-  m_nodeState = nodeState;
-}
+  NodeEnergyModel::~NodeEnergyModel()
+  {
+  }
 
-uint32_t
-NodeEnergyModel::GetNodeOnState (void) const
-{
-  return m_nodeOnState;
-}
+  uint32_t
+  NodeEnergyModel::GetNodeState(void) const
+  {
+    return m_nodeState;
+  }
 
-void
-NodeEnergyModel::SetNodeOnState (uint32_t state)
-{
-  m_nodeOnState = state;
-}
+  void
+  NodeEnergyModel::SetNodeState(uint32_t nodeState)
+  {
+    // NS_LOG_FUNCTION (this << nodeState);
+    m_nodeState = nodeState;
+  }
 
-uint32_t
-NodeEnergyModel::GetNodeOffState (void) const
-{
-  return m_nodeOffState;
-}
+  uint32_t
+  NodeEnergyModel::GetNodeOnState(void) const
+  {
+    return m_nodeOnState;
+  }
 
-void
-NodeEnergyModel::SetNodeOffState (uint32_t state)
-{
-  m_nodeOffState = state;
-}
+  void
+  NodeEnergyModel::SetNodeOnState(uint32_t state)
+  {
+    m_nodeOnState = state;
+  }
 
-void
-NodeEnergyModel::SetNode (Ptr<Node> node)
-{
-  NS_ASSERT (node != NULL);
-  m_node = node;
-}
+  uint32_t
+  NodeEnergyModel::GetNodeOffState(void) const
+  {
+    return m_nodeOffState;
+  }
 
-Ptr<Node>
-NodeEnergyModel::GetNode (void) const
-{
-  return m_node;
-}
+  void
+  NodeEnergyModel::SetNodeOffState(uint32_t state)
+  {
+    m_nodeOffState = state;
+  }
 
-double
-NodeEnergyModel::GetPowerConsumption (void)
-{
-  return 0.0;
-}
+  void
+  NodeEnergyModel::SetNode(Ptr<Node> node)
+  {
+    NS_ASSERT(node != NULL);
+    m_node = node;
+  }
 
-//TODO à modifier pour obtenir un fichier de traces avec les consos
-double
-NodeEnergyModel::GetTotalPowerConsumption (Ptr<Node> node)
-{
-  double conso = 0;
-  conso = GetPowerConsumption (); //chassis consumption
-  if (conso < 0)
+  Ptr<Node>
+  NodeEnergyModel::GetNode(void) const
+  {
+    return m_node;
+  }
+
+  double
+  NodeEnergyModel::GetPowerConsumption(void)
+  {
+    return 0.0;
+  }
+
+  // TODO à modifier pour obtenir un fichier de traces avec les consos
+  double
+  NodeEnergyModel::GetTotalPowerConsumption(Ptr<Node> node)
+  {
+    double conso = 0;
+    conso = GetPowerConsumption(); // chassis consumption
+    if (conso < 0)
     {
-      NS_LOG_UNCOND ("AAA! Negative chassis conso of node " << node->GetId ());
+      NS_LOG_UNCOND("AAA! Negative chassis conso of node " << node->GetId());
     }
-  uint32_t n = node->GetNDevices ();
-  uint32_t i;
-  for (i = 0; i < n; i++)
+    uint32_t n = node->GetNDevices();
+    uint32_t i;
+    for (i = 0; i < n; i++)
     {
-      Ptr<NetDevice> dev = node->GetDevice (i);
-      Ptr<NetdeviceEnergyModel> edev = dev->GetObject<NetdeviceEnergyModel> ();
-      //NS_LOG_UNCOND ("NetDev " << i);
-      if ((edev != NULL) && (dev->GetInstanceTypeId ().GetName () != "ns3::LoopbackNetDevice"))
+      Ptr<NetDevice> dev = node->GetDevice(i);
+      Ptr<NetdeviceEnergyModel> edev = dev->GetObject<NetdeviceEnergyModel>();
+      // NS_LOG_UNCOND ("NetDev " << i);
+      if ((edev != NULL) && (dev->GetInstanceTypeId().GetName() != "ns3::LoopbackNetDevice"))
+      {
+        double temp = edev->GetPowerConsumption(); // consumption of the net devices
+        conso += temp;
+        if (temp < 0)
         {
-          double temp = edev->GetPowerConsumption (); // consumption of the net devices
-          conso += temp;
-          if (temp < 0)
-            {
-              NS_LOG_UNCOND ("AAA! Negative port conso on node " << node->GetId () << " netdev "
-                                                                 << i);
-            }
+          NS_LOG_UNCOND("AAA! Negative port conso on node " << node->GetId() << " netdev "
+                                                            << i);
         }
+      }
     }
-  return conso;
-}
+    return conso;
+  }
 
-void
-NodeEnergyModel::UpdateEnergy (Ptr<Node> node)
-{
-  Time t_now = Simulator::Now ();
-  m_lastConso = GetTotalPowerConsumption (node);
-  m_powerDrawn += m_lastConso * (t_now - m_lastUpdate).GetSeconds ();
-  m_lastUpdate = t_now;
-}
+  void
+  NodeEnergyModel::UpdateEnergy(Ptr<Node> node)
+  {
+    Time t_now = Simulator::Now();
+    m_lastConso = GetTotalPowerConsumption(node);
+    m_powerDrawn += m_lastConso * (t_now - m_lastUpdate).GetSeconds();
+    m_lastUpdate = t_now;
+  }
 
-void
-NodeEnergyModel::LogTotalPowerConsumption (Ptr<Node> node, Ptr<OutputStreamWrapper> streamWrapper)
-{
-  std::string nodeName = Names::FindName (node);
+  void
+  NodeEnergyModel::LogTotalPowerConsumption(Ptr<Node> node, Ptr<OutputStreamWrapper> streamWrapper)
+  {
+    UpdateEnergy(node);
 
-  std::ostream *stream = streamWrapper->GetStream ();
-  *stream << Simulator::Now ().GetSeconds () << ";" << nodeName << ";" << m_lastConso << "\n";
-}
+    std::string nodeName = Names::FindName(node);
 
-void
-NodeEnergyModel::GetConsoLog (Time interval, Time stop, Ptr<Node> node,
-                              Ptr<OutputStreamWrapper> streamWrapper)
-{
-  Time i = Seconds (0.0);
-  while (i <= stop)
+    std::ostream *stream = streamWrapper->GetStream();
+    *stream << std::fixed << Simulator::Now().GetSeconds() << ";" << nodeName << ";" << m_lastConso << "\n";
+  }
+
+  void
+  NodeEnergyModel::GetConsoLog(Time interval, Time stop, Ptr<Node> node,
+                               Ptr<OutputStreamWrapper> streamWrapper)
+  {
+    Time i = Seconds(0.0);
+    while (i <= stop)
     {
-      Simulator::Schedule (i, &NodeEnergyModel::LogTotalPowerConsumption, this, node,
-                           streamWrapper);
+      Simulator::Schedule(i, &NodeEnergyModel::LogTotalPowerConsumption, this, node,
+                          streamWrapper);
       i += interval;
     }
-}
+  }
 
-void
-NodeEnergyModel::GetConso (Time interval, Time stop, Ptr<Node> node)
-{
-  Time i = Seconds (0.0);
-  while (i <= stop)
+  void
+  NodeEnergyModel::GetConso(Time interval, Time stop, Ptr<Node> node)
+  {
+    Time i = Seconds(0.0);
+    while (i <= stop)
     {
-      Simulator::Schedule (i, &NodeEnergyModel::UpdateEnergy, this, node);
+      Simulator::Schedule(i, &NodeEnergyModel::UpdateEnergy, this, node);
       i += interval;
     }
-}
+  }
 
-double
-NodeEnergyModel::GetPowerDrawn ()
-{
-  return m_powerDrawn / 3600; // To Wh
-}
+  double
+  NodeEnergyModel::GetPowerDrawn()
+  {
+    return m_powerDrawn / 3600; // To Wh
+  }
 
-double
-NodeEnergyModel::GetCurrentPowerConsumption ()
-{
-  return m_lastConso;
-}
+  double
+  NodeEnergyModel::GetCurrentPowerConsumption()
+  {
+    return m_lastConso;
+  }
 
-double
-NodeEnergyModel::GetPowerPerGB ()
-{
-  Ptr<OFSwitch13Device> of = m_node->GetObject<OFSwitch13Device> ();
-  return m_lastConso / (of->GetCpuLoad ().GetBitRate () / 1e9);
-}
+  double
+  NodeEnergyModel::GetPowerPerGB()
+  {
+    Ptr<OFSwitch13Device> of = m_node->GetObject<OFSwitch13Device>();
+    return m_lastConso / (of->GetCpuLoad().GetBitRate() / 1e9);
+  }
 
-void
-NodeEnergyModel::UpdateState (uint32_t state, double energy, Time duration)
-{
-}
+  void
+  NodeEnergyModel::UpdateState(uint32_t state, double energy, Time duration)
+  {
+  }
 
 } // namespace ns3
